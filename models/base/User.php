d1f06e7e3507e922e6c2638b0e85d54fd7a4ae2a
SoleSS <soless@pjatigorsk.ru>
2019-02-03 21:54:09 +0300

    [fix] missing dependency

---
+++ models/base/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace common\models\base;
+
+/**
+ * User ActiveRecord model.
+ *
+ * @property bool    $isAdmin
+ * @property bool    $isBlocked
+ * @property bool    $isConfirmed
+ *
+ * Database fields:
+ * @property integer $id
+ * @property string  $username
+ * @property string  $email
+ * @property string  $unconfirmed_email
+ * @property string  $password_hash
+ * @property string  $auth_key
+ * @property string  $registration_ip
+ * @property integer $confirmed_at
+ * @property integer $blocked_at
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property integer $last_login_at
+ * @property integer $flags
+ *
+ * Defined relations:
+ * @property Account[] $accounts
+ * @property Profile   $profile
+ *
+ * Dependencies:
+ * @property-read Finder $finder
+ * @property-read Module $module
+ * @property-read Mailer $mailer
+ *
+ * @author Dmitry Erofeev <dmeroff@gmail.com>
+ */
+class User extends dektrium\user\models\User
+{
+
+}
